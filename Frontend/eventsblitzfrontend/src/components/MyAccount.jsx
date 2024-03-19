075c4fc5284627726b2157bd65bca7455c895060
FabihaT <fabiha.tuheen@ucalgary.ca>
2024-03-19 16:33:14 -0600

    Accounts page styled

--- Frontend/eventsblitzfrontend/src/components/MyAccount.jsx
+++ Frontend/eventsblitzfrontend/src/components/MyAccount.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import axios from 'axios';
 import api from '../api/axiosConfig';
+import { Modal } from 'react-bootstrap';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'; // default style
 
 const MyAccount = () => {
   // State variables to store user details, bookings, and events data
@@ -12,6 +14,8 @@ const MyAccount = () => {
   });
   const [bookings, setBookings] = useState([]);
   const [events, setEvents] = useState([]);
+  const [selectedBooking, setSelectedBooking] = useState(null);
+  const [calendarEvents, setCalendarEvents] = useState([]);
 
   useEffect(() => {
     // Retrieve user details from session storage
@@ -35,7 +39,7 @@ const MyAccount = () => {
     // Fetch bookings data for the current user ID
     const fetchBookingsData = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8080/api/bookings/getAllBookings');
+        const response = await api.get('/bookings/getAllBookings');
         // Filter bookings data to include only bookings for the current user ID
         const userBookings = response.data.filter(booking => booking.userID === parseInt(userID));
         setBookings(userBookings);
@@ -50,7 +54,7 @@ const MyAccount = () => {
     // Fetch events data
     const fetchEventsData = async () => {
       try {
-        const response = await axios.get('http://127.0.0.1:8080/api/events/getAllEvents');
+        const response = await api.get('/events/getAllEvents');
         setEvents(response.data);
         console.log('Events Data:', response.data);
       } catch (error) {
@@ -59,7 +63,22 @@ const MyAccount = () => {
     };
 
     fetchEventsData();
-  }, []);
+
+    const fetchCalendarEvents = () => {
+      const eventsForCalendar = bookings.map(booking => {
+        const event = events.find(e => e.eventID === booking.eventID);
+        return {
+          date: event.eventDate,
+          title: event.eventName,
+        };
+      });
+      setCalendarEvents(eventsForCalendar);
+    };
+
+    if (bookings.length > 0 && events.length > 0) {
+      fetchCalendarEvents();
+    }
+  }, [bookings, events]);
 
   const handleDeleteBooking = async (bookingID, eventName, eventDate) => {
     // Confirm cancellation with the user
@@ -67,9 +86,11 @@ const MyAccount = () => {
     if (!confirmCancel) {
       return; // Cancel deletion if user cancels the confirmation
     }
-
+    // Close the modal
+    handleCloseModal();
+    
     try {
-      await axios.delete(`http://127.0.0.1:8080/api/bookings/${bookingID}`);
+      await api.delete(`/bookings/${bookingID}`);
       // Update bookings state after deletion
       setBookings(prevBookings => prevBookings.filter(booking => booking.bookingID !== bookingID));
       //send email confirmation
@@ -85,47 +106,92 @@ const MyAccount = () => {
     }
   };
 
+  const handleTicketClick = (bookingID) => {
+    // Find the associated event using the booking's eventID
+    const bookingWithEventDetails = bookings.find(booking => booking.bookingID === bookingID);
+    const eventDetails = events.find(event => event.eventID === bookingWithEventDetails.eventID);
+  
+    if (eventDetails) {
+      setSelectedBooking({
+        ...bookingWithEventDetails,
+        eventName: eventDetails.eventName,
+        eventDate: eventDetails.eventDate,
+      });
+    } else {
+      console.error('No event details found for booking:', bookingID);
+    }
+  };
+
+  const handleCloseModal = () => {
+    setSelectedBooking(null);
+  };
+
   return (
-    <div style={{ display: 'flex' }}>
-      <div style={{ width: '25%', borderRight: '1px solid #ccc', paddingRight: '10px' }}>
-        <div style={{ textAlign: 'center', marginBottom: '10px' }}>
-          <div style={{ width: '80px', height: '80px', borderRadius: '50%', background: '#ccc', display: 'inline-flex', justifyContent: 'center', alignItems: 'center', fontSize: '24px', fontWeight: 'bold' }}>
+    <div className="container bg-primary-subtle p-4 rounded-5 mt-5">
+      <div className="row justify-content-center">
+        <div className="col-12 text-center user-info mb-3">
+          <div className="profile-logo" style={{ width: '100px', height: '100px', borderRadius: '50%', background: '#ccc', display: 'inline-flex', justifyContent: 'center', alignItems: 'center', fontSize: '48px', fontWeight: 'bold' }}>
             {user.username.charAt(0)}
           </div>
-          <p><strong>{user.username}</strong></p>
+          <div style={{ marginTop: '10px' }}>
+            <p style={{ fontSize: '30px' }}><strong>{user.username}</strong></p>
+            <p style={{ marginTop: '-15px' }}>{user.email}</p>
+          </div>
         </div>
-        <div>
-          {/* <h3>User Information:</h3> */}
-          {/* <p><strong>User ID:</strong> {user.userID}</p> */}
-          <p><strong>Email:</strong> {user.email}</p>
-          {/* Add more user details here */}
-        </div>
-        {/* Additional sections for profile management */}
       </div>
-      <div style={{ width: '75%', paddingLeft: '10px' }}>
-        <h3>User Bookings:</h3>
-        <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-          {bookings.map(booking => {
-            const event = events.find(event => event.eventID === booking.eventID);
-            return (
-              <div key={booking.bookingID} style={{ width: '30%', margin: '10px', borderRadius: '5px', boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', overflow: 'hidden' }}>
-                <div style={{ backgroundColor: '#f8f9fa', padding: '10px' }}>
-                  <h4 style={{ marginBottom: '5px' }}>Event Name: {event ? event.eventName : 'N/A'}</h4>
-                  <p style={{ margin: '5px 0' }}><strong>Event Date:</strong> {event ? new Date(event.eventDate).toLocaleDateString() : 'N/A'}</p>
-                  <p style={{ margin: '5px 0' }}><strong>Seat Number:</strong> {booking.seatNumber}</p>
-                  <p style={{ margin: '5px 0' }}><strong>Insurance Selected:</strong> {booking.insuranceSelected ? 'Yes' : 'No'}</p>
-                  <p style={{ margin: '5px 0' }}><strong>Payment Amount:</strong> ${booking.paymentAmount.toFixed(2)}</p>
-                  {/* Add delete button */}
-                  <button onClick={() => handleDeleteBooking(booking.bookingID, event ? event.eventName : 'N/A', event ? new Date(event.eventDate).toLocaleDateString() : 'N/A')} style={{ marginTop: '10px', backgroundColor: 'red', color: 'white', border: 'none', padding: '5px 10px', cursor: 'pointer', display: 'block', margin: '0 auto', borderRadius: '5px' }}>Cancel Booking</button>
+
+      <div className="row">
+        <div className="col-md-6 mb-3">
+          <h3 className="text-center">My Tickets</h3>
+          <div className="tickets-list" style={{ overflowY: 'auto', maxHeight: '300px' }}>
+            {bookings.map((booking) => {
+              const event = events.find(event => event.eventID === booking.eventID);
+              return (
+                <div key={booking.bookingID} onClick={() => handleTicketClick(booking.bookingID)} style={{ background: 'white', margin: '10px', borderRadius: '5px', padding: '10px', cursor: 'pointer' }}>
+                  <h4 style={{ color: 'purple' }}>{event.eventName}</h4>
+                  <strong><p>{new Date(event.eventDate).toLocaleDateString()}</p></strong>
                 </div>
-              </div>
-            );
-          })}
+              );
+            })}
+          </div>
         </div>
-        {/* Additional sections for booking management */}
+
+      <div className="col-md-5 d-flex justify-content-center">
+        <Calendar
+          style={{ width: '100%' }}
+          tileContent={({ date, view }) => {
+            const event = calendarEvents.find(e => new Date(e.date).toDateString() === date.toDateString());
+            return view === 'month' && event ? (
+              <p style={{ backgroundColor: 'purple', color: '#fff', padding: '5px', borderRadius: '4px', textAlign: 'center' }}>
+                {event.title}
+              </p>
+            ) : null;
+          }}
+        />
       </div>
     </div>
-  );
+  
+      {selectedBooking && (
+        <Modal show={true} onHide={handleCloseModal}>
+          <Modal.Header closeButton>
+            <Modal.Title>Booking Details</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <div>
+              <h4>Event Name: {selectedBooking.eventName}</h4>
+              <p><strong>Event Date:</strong> {new Date(selectedBooking.eventDate).toLocaleDateString()}</p>
+              <p><strong>Seat Number:</strong> {selectedBooking.seatNumber}</p>
+              <p><strong>Insurance Selected:</strong> {selectedBooking.insuranceSelected ? 'Yes' : 'No'}</p>
+              <p><strong>Payment Amount:</strong> ${selectedBooking.paymentAmount.toFixed(2)}</p>
+            </div>
+          </Modal.Body>
+          <Modal.Footer>
+            <button onClick={() => handleDeleteBooking(selectedBooking.bookingID)} style={{ backgroundColor: 'red', color: 'white', border: 'none', padding: '5px 10px', borderRadius: '5px' }}>Cancel Booking</button>
+          </Modal.Footer>
+        </Modal>
+      )}
+    </div>
+  );  
 };
 
 export default MyAccount;
